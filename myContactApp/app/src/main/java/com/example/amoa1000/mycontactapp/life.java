c84b6a3efd30b2e4ce91102ab39dcd04af62fd5b
harry amo <goodudegood1@gmail.com>
2017-05-08 09:18:44 -0700

    r

---
+++ myContactApp/app/src/main/java/com/example/amoa1000/mycontactapp/life.java
@@ -0,0 +1,13 @@
+package com.example.amoa1000.mycontactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class life extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life);
+    }
+}
